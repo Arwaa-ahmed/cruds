f4f781062045798cef42dd1204cc03077aec6696
Arwa <arwaa1295@gmail.com>
2024-09-15 20:52:12 +0300

    add local storage

--- file.js
+++ file.js
@@ -20,3 +20,26 @@ function calc(){
     }
     
 }
+//create product
+let data;
+if(localStorage.product!=null){
+    data=JSON.parse(localStorage.getItem('product'));
+}
+else{
+    data=[];
+}
+create.onclick=function(){
+    let newpro={
+        title:title.value,
+        price:price.value,
+        tax:tax.value,
+        ads:ads.value,
+        discount:discount.value,
+        count:count.value,
+        category:category.value,
+        total:total.innerHTML
+    }
+    data.push(newpro);
+    localStorage.setItem('product',JSON.stringify(data));
+    console.log(data);
+}
