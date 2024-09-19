b9f488ffc334c3d93fc8fd4e70712e66d502dcdd
Arwa <arwaa1295@gmail.com>
2024-09-19 17:13:39 +0300

    delete ubdate and search for data

--- file.js
+++ file.js
@@ -7,6 +7,8 @@ let count=document.getElementById('count');
 let category=document.getElementById('category');
 let create=document.getElementById('create');
 let total=document.getElementById('total');
+let mood ='create';
+let tmp;
 
 function calc(){
     if(price.value!=''){
@@ -39,9 +41,28 @@ create.onclick=function(){
         category:category.value,
         total:total.innerHTML
     }
-    data.push(newpro);
+    if(mood=='create'){
+        if(newpro.count>1){
+            for(let i=0;i<newpro.count;i++){
+                data.push(newpro);
+            }
+        }
+        else{
+            data.push(newpro);
+        }
+    }
+    else{
+        data[tmp]=newpro;
+        mood='create';
+        create.innerHTML='create';
+        count.style.display='block';
+
+    }
+
+   
     localStorage.setItem('product',JSON.stringify(data));
     console.log(data);
+
     clear();
     showdata();
 }
@@ -59,6 +80,7 @@ function clear(){
 }
 //read
 function showdata(){
+    calc();
     let table='';
     for(let i=0;i<data.length;i++){
         table+=`
@@ -66,13 +88,14 @@ function showdata(){
                     <td>${i}</td>
                     <td>${data[i].title}</td>
                     <td>${data[i].price}</td>
-                    <td>${data[i].tax}/td>
                     <td>${data[i].ads}</td>
-                    <td>${data[i].discount}</td>
+                    <td>${data[i].tax}/td>
                     <td>${data[i].total}</td>
+                    <td>${data[i].discount}</td>
                     <td>${data[i].count}</td>
-                    <td><button id="update">update</button></td>
-                    <td><button onclick="deletedata(${i})" id="update">delete</button></td>
+                    <td>${data[i].category}</td>
+                    <td><button id="update" onclick="updatedata(${i})">update</button></td>
+                    <td><button onclick="deletedata(${i})" >delete</button></td>
                 </tr>
         `
     }
@@ -80,7 +103,7 @@ function showdata(){
      let btn=document.getElementById('btn')
      if(data.length>0){
         btn.innerHTML=`
-        <button onclick="deleteall()">Delete all</button>
+        <button onclick="deleteall()">Delete all ${data.length}</button>
         `
      }
      else{
@@ -101,4 +124,81 @@ function deleteall(){
     data.splice(0);
     showdata();
 
+}
+//update data
+function updatedata(i){
+    title.value=data[i].title;
+    price.value=data[i].price;
+    ads.value=data[i].ads;
+    tax.value=data[i].tax;
+    discount.value=data[i].discount;
+    count.value=data[i].count;
+    count.style.display='none';
+    create.innerHTML='update';
+    mood='update';
+    tmp=i;
+    calc();
+    scroll({
+        top:0
+    })
+}
+
+//search
+let searchmode='title';
+function getsearch(id){
+    if(id=='serchtitle'){
+        searchmode='title';
+        search2.placeholder="search by title";
+    }else{
+        searchmode='category';
+        search2.placeholder='search by category';
+    }
+    search2.focus();
+}
+
+function searchdata(value){
+    let table='';
+    if(searchmode=='title'){
+        for(let i=0;i<data.length;i++){
+            if(data[i].title.includes(value.toLowerCase())){
+                table+=`
+                <tr>
+                          <td>${i}</td>
+                          <td>${data[i].title}</td>
+                          <td>${data[i].price}</td>
+                          <td>${data[i].ads}</td>
+                          <td>${data[i].tax}/td>
+                          <td>${data[i].total}</td>
+                          <td>${data[i].discount}</td>
+                          <td>${data[i].count}</td>
+                           <td>${data[i].category}</td>
+                          <td><button id="update" onclick="updatedata(${i})">update</button></td>
+                          <td><button onclick="deletedata(${i})" >delete</button></td>
+                      </tr>
+              `
+            }
+        }
+    }
+    else{
+        for(let i=0;i<data.length;i++){
+            if(data[i].category.includes(value.toLowerCase())){
+                table+=`
+                <tr>
+                          <td>${i}</td>
+                          <td>${data[i].title}</td>
+                          <td>${data[i].price}</td>
+                          <td>${data[i].ads}</td>
+                          <td>${data[i].tax}/td>
+                          <td>${data[i].total}</td>
+                          <td>${data[i].discount}</td>
+                          <td>${data[i].count}</td>
+                          <td>${data[i].category}</td>
+                          <td><button id="update" onclick="updatedata(${i})">update</button></td>
+                          <td><button onclick="deletedata(${i})" >delete</button></td>
+                      </tr>
+              `
+            }
+        }
+    }
+    document.getElementById('tbody').innerHTML=table;  
 }
